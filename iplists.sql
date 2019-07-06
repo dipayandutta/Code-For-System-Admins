3222dd7fb3dc7dd52968761b9e40e12f44a71a51
Dipayan <inbox.dipayan@gmail.com>
2019-07-06 07:45:08 -0400

    sample ip database

---
+++ iplists.sql
@@ -0,0 +1,20 @@
+create table iplists (
+	id INT,
+	name VARCHAR(50),
+	ip VARCHAR(20)
+);
+insert into iplists (id, name, ip) values (1, 'ebay.com', '71.178.246.123');
+insert into iplists (id, name, ip) values (2, 'sourceforge.net', '255.42.14.85');
+insert into iplists (id, name, ip) values (3, 'joomla.org', '68.247.28.174');
+insert into iplists (id, name, ip) values (4, 'amazon.co.jp', '240.202.194.56');
+insert into iplists (id, name, ip) values (5, 'washingtonpost.com', '42.47.108.167');
+insert into iplists (id, name, ip) values (6, 'army.mil', '36.158.87.23');
+insert into iplists (id, name, ip) values (7, 'blog.com', '53.66.142.52');
+insert into iplists (id, name, ip) values (8, 'sun.com', '115.33.147.149');
+insert into iplists (id, name, ip) values (9, 'netlog.com', '181.119.75.123');
+insert into iplists (id, name, ip) values (10, 'github.io', '108.74.196.251');
+insert into iplists (id, name, ip) values (11, 'psu.edu', '227.60.178.154');
+insert into iplists (id, name, ip) values (12, 'jugem.jp', '189.179.73.102');
+insert into iplists (id, name, ip) values (13, 'netscape.com', '82.118.64.73');
+insert into iplists (id, name, ip) values (14, 'bigcartel.com', '255.220.203.222');
+insert into iplists (id, name, ip) values (15, 'yelp.com', '49.223.21.65');
